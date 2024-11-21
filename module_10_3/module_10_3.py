75a12e0bcf52ffcfd4c5a8b056f3b8a9b98cdf39
Denis <vin_ogr@list.ru>
2024-11-21 16:09:02 +0700

    Блокировки и обработка ошибок

---
+++ module_10_3/module_10_3.py
@@ -0,0 +1,23 @@
+import threading
+
+
+class Bank:
+    lock = Lock()
+    def __init__(self):
+        pass
+    def deposit(self):
+        pass
+    def take(self):
+        pass
+
+bk = Bank()
+
+th1 = (threading.Thread(target=Bank.deposit, args=(bk,)))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
